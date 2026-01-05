6043e869fab1d53d1836f23e66c057a91ec50d64
minhdc-win <daochiminh@cybersoft.com.vn>
2026-01-05 15:12:00 +0700

    u

--- lib/Module/Info/deviceinfo.dart
+++ lib/Module/Info/deviceinfo.dart
@@ -11,6 +11,15 @@ class DeviceInfo extends ChangeNotifier {
   static Future<void> setservername(String value) async =>
       await AppStorage.set("servername", value);
 
+  static Future<String> get tencty async => await AppStorage.get("tencty");
+  static Future<void> settencty(String value) async =>
+      await AppStorage.set("tencty", value);
+
+  static Future<String> get urlBanner async =>
+      await AppStorage.get("urlBanner");
+  static Future<void> seturlBanner(String value) async =>
+      await AppStorage.set("urlBanner", value);
+
   static Future<String> get macdevice async =>
       await AppStorage.get("macdevice");
   static Future<void> setmacdevice(String value) async =>
@@ -19,15 +28,20 @@ class DeviceInfo extends ChangeNotifier {
   static Future<String> get cetificate async {
     String cer = await AppStorage.get("cetificate");
     if (cer == "") {
-      var uuid = Uuid();
-      cer = uuid.v1();
-      await setcetificate(cer);
+      cer = manufacturer();
+      if (cer == "") {
+        var uuid = Uuid();
+        cer = uuid.v1();
+        await setcetificate(cer);
+      }
     }
-
     return cer;
   }
 
   static Future<void> setcetificate(String value) async =>
       await AppStorage.set("cetificate", value);
-  //cetificate
+  static String displayDeviceName() => CyberDeviceInfo().displayDeviceName;
+  static String deviceName() => CyberDeviceInfo().deviceName;
+  static String deviceId() => CyberDeviceInfo().deviceId;
+  static String manufacturer() => CyberDeviceInfo().manufacturer;
 }
