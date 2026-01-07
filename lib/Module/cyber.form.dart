968681dd10b5c705f34aac3fbfe1ffa8e2af5991
minhdc-win <daochiminh@cybersoft.com.vn>
2026-01-07 14:39:58 +0700

    u

--- lib/Module/cyber.form.dart
+++ lib/Module/cyber.form.dart
@@ -367,7 +367,7 @@ abstract class CyberForm {
   bool? _hideAppBar;
   bool? _showSpeedMonitor;
   bool? _hideBottomNavigationBar;
-
+  Color? XanhChu = Color(0xFF0F3D34);
   // Getters với fallback cho override
   String? get title => _title;
   set title(String? value) => _title = value;
